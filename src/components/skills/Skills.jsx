95b39bdd4fd035c14966c7e966a22c35ddab2b6f
samyu Lakshmi <samyu.lakshmi123@gmail.com>
2024-06-24 21:17:28 +0530

    icon changed

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './skills.css'
-import { TbDiscountCheckFilled } from "react-icons/tb";
+import { TbRosetteDiscountCheck } from "react-icons/tb";
 
 function Skills() {
   return (
@@ -13,42 +13,42 @@ function Skills() {
           <h3>Frontend Development</h3>
           <div className="skills__content">
             <article className="skills__details">
-              <TbDiscountCheckFilled className='skills__details-icon' />
+              <TbRosetteDiscountCheck className='skills__details-icon' />
               <div>
                 <h4>HTML</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className="skills__details">
-              <TbDiscountCheckFilled className='skills__details-icon'/>
+              <TbRosetteDiscountCheck className='skills__details-icon'/>
               <div>
                 <h4>CSS</h4>
                 <small className='text-light'>Intermediate</small>
               </div>
             </article>
             <article className="skills__details">
-              <TbDiscountCheckFilled className='skills__details-icon'/>
+              <TbRosetteDiscountCheck className='skills__details-icon'/>
               <div>
                 <h4>JavaScript</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className="skills__details">
-              <TbDiscountCheckFilled className='skills__details-icon'/>
+              <TbRosetteDiscountCheck className='skills__details-icon'/>
               <div>
                 <h4>BootStrap</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className="skills__details">
-              <TbDiscountCheckFilled className='skills__details-icon'/>
+              <TbRosetteDiscountCheck className='skills__details-icon'/>
               <div>
                 <h4>DHTMLX</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className="skills__details">
-              <TbDiscountCheckFilled className='skills__details-icon'/>
+              <TbRosetteDiscountCheck className='skills__details-icon'/>
               <div>
                 <h4>React</h4>
                 <small className='text-light'>Intermediate</small>
@@ -60,42 +60,42 @@ function Skills() {
         <h3>Backend Development</h3>
           <div className="skills__content">
             <article className="skills__details">
-              <TbDiscountCheckFilled className='skills__details-icon'/>
+              <TbRosetteDiscountCheck className='skills__details-icon'/>
               <div>
                 <h4>MongoDB</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className="skills__details">
-              <TbDiscountCheckFilled className='skills__details-icon'/>
+              <TbRosetteDiscountCheck className='skills__details-icon'/>
               <div>
                 <h4>C++</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className="skills__details">
-              <TbDiscountCheckFilled className='skills__details-icon'/>
+              <TbRosetteDiscountCheck className='skills__details-icon'/>
               <div>
                 <h4>MySQL</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className="skills__details">
-              <TbDiscountCheckFilled className='skills__details-icon' />
+              <TbRosetteDiscountCheck className='skills__details-icon' />
               <div>
                 <h4>Java</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className="skills__details">
-              <TbDiscountCheckFilled className='skills__details-icon' />
+              <TbRosetteDiscountCheck className='skills__details-icon' />
               <div>
                 <h4>Python</h4>
                 <small className='text-light'>Intermediate</small>
               </div>
             </article>
             <article className="skills__details">
-              <TbDiscountCheckFilled className='skills__details-icon' />
+              <TbRosetteDiscountCheck className='skills__details-icon' />
               <div>
                 <h4>Hibernate</h4>
                 <small className='text-light'>Experienced</small>
